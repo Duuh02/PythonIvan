41b1613c0fe41835b08987ebb77a6258f85f5033
Duuh02 <163295805+Duuh02@users.noreply.github.com>
2024-10-15 12:06:20 -0300

    as

---
+++ atividade.py
@@ -0,0 +1,8 @@
+print("Bem Vindo a tela de Cadastro")
+def Cadastro_pessoas():
+nome = print(input("Digite seu Nome: "))
+
+idade = print(input("Digite Sua Idade: "))
+print(idade)
+
+cpf = print(input("Digite Seu CPf"))
